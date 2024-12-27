2da75989b5a3517f7a84345261f9ef85d5567c23
Pablo Bustos <pablo.bustos@chainalysis.com>
2024-12-27 07:47:26 -0600

    not working2

--- test.rb
+++ test.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
-require 'open-uri'
-require 'logger'
+require 'uri'
+require 'net/http'
 
+require 'logger'
 $logger = Logger.new(STDOUT)
 $logger.info 'starting'
 
@@ -10,8 +11,18 @@ K8S_APISERVER = 'https://kubernetes.default.svc'
 K8S_SERVICEACCOUNT = '/var/run/secrets/kubernetes.io/serviceaccount'
 K8S_TOKEN = File.read("#{K8S_SERVICEACCOUNT}/token")
 
-response = URI.open("#{K8S_APISERVER}/api/v1/namespaces/operb/pods?watch=1&resourceVersion=7069965",
-    :ssl_ca_cert => "#{K8S_SERVICEACCOUNT}/ca.crt",
-    'Authorization' => "Bearer #{K8S_TOKEN}" )
-$logger.info response
+url = URI.parse("#{K8S_APISERVER}/api/v1/namespaces/operb/pods?watch=1&resourceVersion=7069965")
+http = Net::HTTP.new(url.host, url.port)
+http.ca_file = "#{K8S_SERVICEACCOUNT}/ca.crt"
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+request = Net::HTTP::Get.new(url.path)
+request.add_field 'Authorization', "Bearer #{K8S_TOKEN}"
+http.request(request) do |response|
+  response.read_body do |chunk|
+    $logger.info chunk.size
+    $logger.info chunk
+  end
+end
+# $logger.info response.body if response.is_a?(Net::HTTPSuccess)
 $logger.info 'done'
