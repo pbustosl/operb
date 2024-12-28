57eb91c33aab98bcb46816aa0c09d7ecea0983b0
Pablo Bustos <pablo.bustos@chainalysis.com>
2024-12-27 18:08:50 -0600

    refactoring

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'typhoeus'
-
+require 'json'
 require 'logger'
 $logger = Logger.new(STDOUT)
 $logger.info 'starting'
@@ -10,31 +10,34 @@ K8S_APISERVER = 'https://kubernetes.default.svc'
 K8S_SERVICEACCOUNT = '/var/run/secrets/kubernetes.io/serviceaccount'
 K8S_TOKEN = File.read("#{K8S_SERVICEACCOUNT}/token")
 
-url = "#{K8S_APISERVER}/api/v1/namespaces/operb/pods"
-url = "#{K8S_APISERVER}/api/v1/namespaces/operb/pods?watch=1&resourceVersion=7829819"
-request = Typhoeus::Request.new(url,
+resource_path = "/api/v1/namespaces/operb/pods"
+req_opts = {
   cainfo: "#{K8S_SERVICEACCOUNT}/ca.crt",
   headers: { Authorization: "Bearer #{K8S_TOKEN}" }
-  )
+}
+
+url = "#{K8S_APISERVER}#{resource_path}"
+request = Typhoeus::Request.new(url, req_opts)
+request.run
+response = request.response
+raise "Request failed" if response.code != 200
+pod_list = JSON.parse(response.body)
+resourceVersion = pod_list['metadata']['resourceVersion']
+$logger.info "resourceVersion=#{resourceVersion}"
+
+url = "#{K8S_APISERVER}#{resource_path}?watch=1&resourceVersion=#{resourceVersion}"
+request = Typhoeus::Request.new(url, req_opts)
 request.on_headers do |response|
-  $logger.info "response.code: #{response.code}"
-  if response.code != 200
-    raise "Request failed"
-  end
+  $logger.info "on_headers response.code=#{response.code}"
+  raise "Request failed" if response.code != 200
 end
 request.on_body do |chunk|
-    $logger.info chunk.size
-    $logger.info chunk
+    $logger.info "chunk.size=#{chunk.size}"
+    $logger.info "chunk=#{chunk}"
 end
 request.on_complete do |response|
   $logger.info 'on_complete'
-  $logger.info "response.code: #{response.code}"
-  $logger.info "response.body: #{response.body}"
 end
+$logger.info 'watch'
 request.run
-response = request.response
-$logger.info "response.code: #{response.code}"
-$logger.info "response.body: #{response.body}"
-$logger.info response
-sleep 300
 $logger.info 'done'
